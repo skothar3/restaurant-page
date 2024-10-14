a2cc173be1109ccadf82135fa242572a9e0d9029
Sid Kothari <sid.kothari7@gmail.com>
2024-10-14 16:32:05 -0400

    Add html-loader and html-webpack-plugin as dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -6,12 +7,26 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.html',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+	test: /\.html$/i,
+	loader: "html-loader",
       },
     ],
   },
